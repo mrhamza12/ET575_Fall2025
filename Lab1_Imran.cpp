6214d7435c7aef9ab9758797966618d33a83d1f2
mrhamza12 <hamzaimranpakistan6@gmail.com>
2025-08-27 20:02:08 +0000

    lab1

---
+++ Lab1_Imran.cpp
@@ -0,0 +1,37 @@
+/*
+Student's full name 
+Aug 27, 2025 
+Lab 1, 
+/*
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "------Example 1----"<<endl;
+    cout<<"Machine"<<endl;
+    // variables + temporary strorage of data
+    // deleare the variable first
+    string username = "peterpan123";
+    cout<<"Welcome to class"<<username<<endl;
+    
+    cout << "------Example 2: cin data----"<<endl;
+    cout<<"Enter a new username"<<endl;
+    cin >> username;
+    cout<<"New user has is "<<username<<endl;
+    // declear variable
+    char sym = '%' ;
+    float distance = 5.63;
+    int number = 8.2;
+
+    cout<<"Given symbol = "<<sym<<endl;
+    cout<<"Given distance ="<<distance<<endl;
+    cout<<"Given number ="<<number<<endl;
+    
+    cout<< "-------Exercise----"<<end;
+
+    //declare variable
+    //
+
+    return 0;
+}
