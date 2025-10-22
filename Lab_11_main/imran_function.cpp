3f51ba5b87e8df3d89447e585d73afbfcfd35887
mrhamza12 <hamzaimranpakistan6@gmail.com>
2025-10-22 20:11:00 +0000

    Lab 11 Completed

---
+++ Lab_11_main/imran_function.cpp
@@ -0,0 +1,16 @@
+/*
+student"s hamza imran
+Oct 22, 2025
+lab 11,Intriduction to function
+*/
+#include<iostream>
+using namespace std;
+
+// void functioin does not return any value
+void printhello;
+    cout<<"Hello function"<<endl;
+
+
+
+    return 0;
+}
