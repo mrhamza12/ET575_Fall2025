b6c4f05248b1c6f211959eb769c4e3379761fa49
mrhamza12 <hamzaimranpakistan6@gmail.com>
2025-11-13 18:13:09 +0000

    lab 15 done

--- Lab15_array.cpp/lab15_function_imran.cpp
+++ Lab15_array.cpp/lab15_function_imran.cpp
@@ -3,108 +3,131 @@ Hamza imran
 Nov 12, 2025
 lab 14: introduction to arrays, pointers and references
 */
-#include <string>
 #include <iostream>
-
+#include <string>
 using namespace std;
 
+// ---------------- Example 1: pointers ----------------
 void pointer_ref(){
     int num = 12;
     char c = 'T';
     string n = "Peter";
 
-    // declare the pointer
     int* pointerint;
     char* pointerchar;
     string* pointerstring;
 
-    // testing the memory address
-    cout<<"The memory address of num is: "<<pointerint << endl;
+    cout << "memory address of int = " << pointerint << endl;
 
-    // pointer with initial value. Value is the memory address of the pointed variable
+    // assign addresses
     pointerint = &num;
     pointerchar = &c;
     pointerstring = &n;
 
-    //check reference value of each pointer
-    cout<<"Reference value int = "<<*pointerint<<endl;
-    cout<<"Reference value char = "<<*pointerchar<<endl;
-    cout<<"Reference value string = "<<*pointerstring<<endl;
+    cout << "Reference value int = " << pointerint << endl;
+    cout << "Reference value char = " << pointerchar << endl;
+    cout << "Reference value string = " << pointerstring << endl;
 
-    //defreference operators
-    cout<<"Dereference value of int "<<*pointerint<<endl;
-    cout<<"Dereference value of char "<<*pointerchar<<endl;
-    cout<<"Dereference value of string "<<*pointerstring<<endl;
+    // dereference
+    cout << "Dereference value of int = " << *pointerint << endl;
+    cout << "Dereference value of char = " << *pointerchar << endl;
+    cout << "Dereference value of string = " << *pointerstring << endl;
 }
 
-// example 2
-// function that passes a value
+
+// ---------------- Example 2: functions ----------------
 void printvalue(string v){
-    cout<<"Value = "<<v<<endl;
+    cout << "Value = " << v << endl;
+    v = "inside printvalue function";
 }
 
-// function that passes by reference
-void printbyreference(string& v){
-    cout<<"Passed by reference = \t"<<v<<endl;
+void printbyreference(string &v){
+    cout << "Passed by reference =\t" << v << endl;
+    v = "Updated sentence";
 }
 
-//function that passes a memory address
-void printbypointer(string* v){
-    cout<<"Pass memory address = \t"<<v<<endl;
+void printaddress(string* v){
+    cout << "Pass memory address =\t" << v << endl;
 }
 
-// example 3: arrays
+
+// ---------------- Example 3: arrays ----------------
 void arrays(){
-    // declare an array with five values
     int scores[5];
-
-    //assigning/accessing individual cells in an array
     scores[0] = 60;
     scores[1] = 30;
     scores[2] = 75;
     scores[3] = 79;
     scores[4] = 98;
-     cout<<"Array = \t"<<scores<<endl;
-     cout<<"The third score is "<<scores[2]<<endl;
 
-    // initializing values to an array
-     int age_children[3] = {12,8,5};
-     cout<<"The 1st age = "<<age_children[0]<<endl;
+    cout << "Array =\t" << scores << endl;
+    cout << "The third score is " << scores[2] << endl;
 
-    // when an array is declared with initial, the size of the array can be omitted
-     string names[] = {"Annie","Peter","Charles","Brian"};
-     cout<<"The fourth name is "<<names[3]<<endl;
+    // More array examples
+    int age_children[3] = {12, 8, 5};
+    cout << "The 1st age = " << age_children[0] << endl;
+
+    string names[] = {"Annie", "Peter", "Carl", "Charles", "Brian"};
+    cout << "The fourth name is " << names[3] << endl;
 }
-void sizeofarray(){
-    cout<<"size of char \t"<<sizeof(char)<<endl;
-    cout<<"size of int \t"<<sizeof(int)<<endl;
-    cout<<"size of double \t"<<sizeof(double)<<endl;
-    cout<<"size of string \t"<<sizeof(string)<<endl;
 
-    int age_children[] = {12,8,5};
-    cout<<"Total size of age_children \t"<<sizeof(age_children)<<endl;
-    cout<<"Size of age children \t"<<sizeof(age_children) / sizeof(age_children[0])<<endl;
-}
+
+// ---------------- Example 4: sizeof arrays ----------------
 void sizeofarrays(){
-int age_children[] = {12,8,5};
-cout << "Size of age_children \t" << sizeof(age_children) / sizeof(age_children[0]) << endl;
+
+    cout << "size of char \t" << sizeof(char) << endl;
+    cout << "size of int \t" << sizeof(int) << endl;
+    cout << "size of double \t" << sizeof(double) << endl;
+    cout << "size of string \t" << sizeof(string) << endl;
+
+    int age_children[] = {2, 8, 5};
+    cout << "Total size of age_children \t" << sizeof(age_children) << endl;
+    cout << "Size of age_children \t"
+         << sizeof(age_children) / sizeof(age_children[0]) << endl;
 }
 
-// example 5: loop in an array
+
+// ---------------- Example 5: loop in an array ----------------
 void looparray(){
-string names[] = {"Annie", "Peter", "Carl", "Charles", "Brian"};
-int sizenames = sizeof(names) / sizeof(names[0]);
-int counter = 0;
+    string names[] = {"Annie", "Peter", "Carl", "Charles", "Brian"};
+    int sizenames = sizeof(names) / sizeof(names[0]);
+    int counter = 0;
 
-for(int i = 0; i < sizenames; i++){
-cout << names[i] << "\t";
+    for(int i = 0; i < sizenames; i++){
+        cout << names[i] << "\t";
 
-// count how many names has 5+ characters
-if (names[i].length() >= 5){
-counter++;
+        if(names[i].length() >= 5){
+            counter++;
+        }
+    }
+
+    cout << endl;
+    cout << "There is/are " << counter << " names with 5+ characters!" << endl;
 }
-}
 
-cout << endl;
-cout << "There is/are " << counter << " names with 5+ characters!" << endl;
+// ---------LAB EXERCISE ---------
+void exercise(){
+
+    // declare an array with 5 values
+    int scores[5];
+
+    // assign values to the array
+    scores[0] = 60;
+    scores[1] = 90;
+    scores[2] = 75;
+    scores[3] = 88;
+    scores[4] = 100;
+
+    // set maximum score initially to first element
+    int maxscore = scores[0];
+
+    // loop through array to find maximum score
+    for(int i = 1; i < 5; i++){
+        if(scores[i] > maxscore){
+            maxscore = scores[i];
+        }
+    }
+
+    // print result
+    cout << "The maximum score is " << maxscore << endl;
 }
--- Lab15_array.cpp/lab15_main_imran.cpp
+++ Lab15_array.cpp/lab15_main_imran.cpp
@@ -1,35 +1,41 @@
 /*
-Hamza Imran
-Nov 12, 2025
-lab 14: introduction to arrays, pointers and references
+ Hamza Imran
+ Nov 12, 2025
+ Lab 14: introduction to array, pointers and references
 */
+
 #include <iostream>
 #include <string>
 using namespace std;
+
 #include "lab15_function_imran.cpp"
 
-int main() {
-    std::cout << "\n-----Example 1: Pointers -----" << std::endl;
+int main(){
+
+    cout << "\n------ Example 1: pointers ------" << endl;
     pointer_ref();
 
-    cout << "\n-----Example 2:  reference and pointer in a function -----" << std::endl;
-    string something = "Hello World";
+    cout << "\n------ Example 2: pointers and reference in a function ------" << endl;
+    string something = "Hello World!";
     printvalue(something);
     printbyreference(something);
     printaddress(&something);
     printvalue(something);
     printvalue(something);
 
-
-    std::cout << "\n-----Example 3: Arrays -----" << std::endl;
+    cout << "\n------ Example 3: array ------" << endl;
     arrays();
 
-    std::cout << "\n --- Example 4: array sizes -----" << std::endl;
+    cout << "\n------ Example 4: array sizes ------" << endl;
     sizeofarrays();
 
-    std::cout << "\n --- Example 5: loop in an array  -----" << std::endl;
-    looparrays();
+    cout << "\n------ Example 5: loop in an array ------" << endl;
+    looparray();
+
+    cout << "\n------ LAB EXERCISE ------" << endl;
+    exercise();
 
     return 0;
 }
+
 
